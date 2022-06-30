57a3d8280680ccd2108c20e7cfb05ea8e03cccef
Rami <103028944+Rami24t@users.noreply.github.com>
2022-06-30 13:40:52 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -61,4 +61,4 @@ console.log("Q11: Score in the sixth test: ", 85*6 - (75+70+85+90+100));
 // ((0.8*0.78)+y)/9=0.8
 //
 
-console.log(`Q12: James needs a minimum of ${(0.8*9-0.8*0.78)*100}% to get an 80% average`)
+console.log(`Q12: James needs a minimum of ${(0.8*0.9-0.8*0.78)*100}% to get an 80% average`)
